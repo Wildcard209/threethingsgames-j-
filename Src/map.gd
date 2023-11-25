fa68ae3401ba882766f1e466ea3a2778c1364b12
JamieLAtkinson <142546744+JamieLAtkinson@users.noreply.github.com>
2023-11-25 11:15:05 +0000

    basic stuff done

--- Src/map.gd
+++ Src/map.gd
@@ -1,11 +1,27 @@
-extends Node
+class_name map extends Node
 
-
+var GRID_HORIZ_SIZE : int = 30
+var GRID_VERT_SIZE : int = 30
+var layers = {}
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	for i in eLayers:
+		layers[i] = []
+	for i in range(GRID_VERT_SIZE):
+		layers[eLayers.Base].append([])
+		for j in range(GRID_HORIZ_SIZE):
+			layers[eLayers.Base][j].append(tile.new())
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+enum eLayers{
+	Base,
+	Island,
+	Cosmetic,
+	Building,
+	Entity,
+	Weather
+}
--- Src/tile.gd
+++ Src/tile.gd
@@ -1,11 +1,10 @@
-extends Node
+class_name tile extends CharacterBody2D
 
+var collider = null
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	collider = $CollisionShape2D
+	
+func _physics_process(delta):
+	if 
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
