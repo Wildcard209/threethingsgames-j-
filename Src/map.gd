b4274f8bca770fdf1f09518552a1a1fa3d020ccf
JamieLAtkinson <142546744+JamieLAtkinson@users.noreply.github.com>
2023-11-25 14:03:54 +0000

    make water fully wiggle

--- Src/map.gd
+++ Src/map.gd
@@ -1,32 +1,32 @@
 class_name map extends Node
 
-var GRID_HORIZ_SIZE : int = 2
-var GRID_VERT_SIZE : int = 5
+var GRID_HORIZ_SIZE : int = 31
+var GRID_VERT_SIZE : int = 17
 var TILE_HORIZ_SIZE : int = 64
 var TILE_VERT_SIZE : int = 64
-var layers = []
+var rng = RandomNumberGenerator.new()
+var min_health = 10
+var max_health = 100
+var grid = []
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	for i in eLayers:
-		layers.append([])
 	for i in GRID_HORIZ_SIZE:
-		layers[eLayers.Base].append([])
+		grid.append([])
 		for j in GRID_VERT_SIZE:
 			var t = preload("res://Src/tile/tile.tscn").instantiate()
 			t.transform = Transform2D(0.0,Vector2(i*TILE_HORIZ_SIZE,j*TILE_VERT_SIZE))
+			t.hp = rng.randi_range(min_health,max_health)
 			add_child(t)
-			layers[eLayers.Base][i].append(t)
-
+			grid[i].append(t)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
-
-enum eLayers{
-	Base,
-	Island,
-	Cosmetic,
-	Building,
-	Entity,
-	Weather
-}
+	for i in GRID_HORIZ_SIZE:
+		for j in GRID_VERT_SIZE:
+			var x =grid[i][j]
+			var a = clamp(i+1,0,GRID_HORIZ_SIZE-1)
+			var b = clamp(i-1,0,GRID_HORIZ_SIZE-1)
+			var c = clamp(j+1,0,GRID_VERT_SIZE-1)
+			var d = clamp(j-1,0,GRID_VERT_SIZE-1)
+			if grid[a][j].isWater() or grid[b][j].isWater() or grid[i][c].isWater() or grid[i][d].isWater():
+				x.erode(delta)
--- Src/tile/tile.gd
+++ Src/tile/tile.gd
@@ -3,16 +3,47 @@ class_name tile extends Node2D
 var hp : int = 100
 var buildState : eBuildState = eBuildState.none
 var collider = null
+var hovered = false
 
 
 func _ready():
-	pass
+	var anim = $AnimatedSprite2D
+	anim.play()
 
 func _on_area_2d_area_entered(area):
 	if area.owner.get_class() == "tsunami":
 		pass
+		
+func _on_area_2d_mouse_entered():
+	var rect = $ColorRect
+	hovered = true
+	rect.visible = true
+
+func _on_area_2d_mouse_exited():
+	var rect = $ColorRect
+	hovered = false
+	rect.visible = false
+
+func _input(event):
+	if hovered:
+		if Input.is_action_just_pressed("test"):
+			pass
+
+func isWater():
+	if buildState == eBuildState.dead:
+		return true
+	else:
+		return false
+
+func erode():
+	pass
 
 enum eBuildState{
 	none,
+	dead,
 	windmill
 }
+
+
+
+
