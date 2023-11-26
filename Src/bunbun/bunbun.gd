39cba1ecae18ee8048ac2c6ecf68da0c01804aff
JamieLAtkinson <142546744+JamieLAtkinson@users.noreply.github.com>
2023-11-26 02:11:27 +0000

    imports

--- Src/bunbun/bunbun.gd
+++ Src/bunbun/bunbun.gd
@@ -2,11 +2,17 @@ extends CharacterBody2D
 # Get the gravity from the project settings to be synced with RigidBody nodes
 
 var speed = 20
+
+func _ready():
+	$AnimatedSprite2D.play()
+
 func _physics_process(delta):
 	# Add the gravity.
-	var direction = -get_local_mouse_position()
-	position += delta*speed*direction
-	pass
+	var direction = -get_local_mouse_position().normalized()
+	velocity = speed *direction
+	print(position)
+	move_and_slide()
 	# Handle Jump.
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
+
